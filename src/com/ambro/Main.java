950b36c3ebf5c245a262d6294a9330e2c43db77e
Kamzs <kamilambroziak@interia.pl>
2019-11-06 20:55:58 +0100

    save

--- src/com/ambro/Main.java
+++ src/com/ambro/Main.java
@@ -6,14 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //VectorFix
+/*        //VectorFix
         VectorFix vectorFix = new VectorFix();
         vectorFix.push(1);
         vectorFix.push(1);
         vectorFix.push(1);
-        vectorFix.push(1);
+        vectorFix.push(1);*/
 
-        System.out.println(vectorFix.toString());
+        //     System.out.println(vectorFix.toString());
 
         //VectorFloat
         VectorFloat vectorFloat = new VectorFloat();
@@ -24,10 +24,9 @@ public class Main {
         vectorFloat.push(1);
         vectorFloat.push(1);
 
-        System.out.println(vectorFloat.toString());
+        //    System.out.println(vectorFloat.toString());
 
         //VectorObjectFloat
-
         SomeClass someClass1 = new SomeClass();
         SomeClass someClass2 = new SomeClass();
         SomeClass someClass3 = new SomeClass();
@@ -37,38 +36,60 @@ public class Main {
         vectorOfObjects.push(someClass2);
         vectorOfObjects.push(someClass3);
 
-        System.out.println(vectorOfObjects.toString());
+        // System.out.println(vectorOfObjects.toString());
 
         //graphAdjencyList
+        GraphAdjencyList graphAdjencyList = new GraphAdjencyList(10);
 
-        GraphAdjencyList graphAdjencyList = new GraphAdjencyList(4);
-
-        graphAdjencyList.addEdge(0, 2);
-        graphAdjencyList.addEdge(0, 3);
+        graphAdjencyList.addEdge(0, 1);
+        graphAdjencyList.addEdge(1, 2);
         graphAdjencyList.addEdge(2, 3);
-        graphAdjencyList.addEdge(3, 2);
-        graphAdjencyList.addEdge(3, 1);
-        graphAdjencyList.addEdge(2, 1);
+        graphAdjencyList.addEdge(3, 4);
+        graphAdjencyList.addEdge(4, 5);
+        graphAdjencyList.addEdge(5, 6);
+        graphAdjencyList.addEdge(2, 6);
 
+        Main main = new Main();
+        System.out.println(main.BFSLowestDist(graphAdjencyList, 0, 6));
 
     }
 
-
     void BFS(GraphAdjencyList G, int start) {
-        QueFIFOUsingVectorFloat Q = new QueFIFOUsingVectorFloat();
+        VectorFloat VectorZPOPLIFO = new VectorFloat();
         boolean[] visited = new boolean[G.N];
 
         visited[start] = true;
-        Q.push_back(start);
-        while (Q.back > 0) { //back? count? wtf
-            int u = Q.pop_front();
-            for (int i = 0; i < G.nodes[u].count; i++) {
-                int v = G.nodes[u].data[i];
-                if (visited[v]) continue;
+        VectorZPOPLIFO.push(start);
+        while (VectorZPOPLIFO.count > 0) {
+            int nodeToCheck = VectorZPOPLIFO.pop();
+            for (int i = 0; i < G.nodes[nodeToCheck].count; i++) {
+                int nodeConnected = G.nodes[nodeToCheck].get(i);
+                if (visited[nodeConnected]) continue;
                 //logic
-                visited[v] = true;
-                Q.push_back(v);
+                visited[nodeConnected] = true;
+                VectorZPOPLIFO.push(nodeConnected);
             }
         }
+    }
+
+    int BFSLowestDist(GraphAdjencyList G, int start, int end) {
+        VectorFloat VectorZPOPLIFO = new VectorFloat();
+        int[] dist = new int[G.N];
+
+        dist[start] = 1;
+        VectorZPOPLIFO.push(start);
+        while (VectorZPOPLIFO.count > 0) {
+            int nodeToCheck = VectorZPOPLIFO.pop();
+            for (int i = 0; i < G.nodes[nodeToCheck].count; i++) {
+                int nodeConnected = G.nodes[nodeToCheck].get(i);
+                if (dist[nodeConnected] > 0) continue;
+                dist[nodeConnected] = dist[nodeToCheck] + 1;
+                if (nodeConnected == end) {
+                    return dist[nodeConnected] - 1;
+                }
+                VectorZPOPLIFO.push(nodeConnected);
+            }
+        }
+        return -1;
     }
 }
